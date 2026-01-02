ae4a3cab0f9b1528715b2d6494c7aa0a9bee883c
Simon Parten <quafadas@gmail.com>
2026-01-02 20:34:09 +0100

    .

---
+++ README.md
@@ -0,0 +1,7 @@
+Running
+
+`mill mill.scalalib.scalafmt/`
+
+Does format the code in `hi.scala`. But not `build.mill` or `sub/package.mill`.
+
+Running scalafmt directly on those files does format them.
---
+++ submod/src/addit.scala
@@ -0,0 +1,18 @@
+package submod
+
+import testing.testing
+
+object addit:
+  def plusly(a: Int, b: Int) = a + b
+
+  testing {
+    class PluslySuite extends munit.FunSuite:
+          test("plusly") {
+            assertEquals(plusly(20, 23), 43)
+          }
+  }
+end addit
+
+@main def runAddit() =
+  println(s"2 + 3 = ${addit.plusly(2, 3)}")
+
